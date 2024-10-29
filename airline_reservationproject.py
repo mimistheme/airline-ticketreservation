e5599ad1ab58063db4eb20fb49734b542502ceb2
Marie N’Lemvo <marienlemvo@Maries-MacBook-Air.local>
2024-10-29 15:08:40 +0000

    changes

--- airline_reservationproject.py
+++ airline_reservationproject.py
@@ -1,10 +1,23 @@
 import csv
 # function to write into csv file
 def write():
-    with open('customerid_db.csv', mode='w') as db_write:
-        rows = csv.writer(db_write, delimiter=',', quoting=csv.QUOTE_MINIMAL, qoutechar='"')
-        rows.writerow(['Marie','NLemvo','350','256789','6:00am','13n','economy','active'
-])
+    with open('cusid_db.csv', mode='w') as db_write:
+        rows = csv.writer(db_write, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        rows.writerow(['Marie-at','NLemvo','350','256789','6:00am','13n','economy','active'])
+#run the function first before reading
+write()
 # function to read
-with open ('customerid_db.csv', mode='r') as db_read:
-    pass
+with open ('cusid_db.csv', mode='r') as db_read:
+    csv_reader = csv.reader(db_read, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {" | ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} {row[1]} ticket number {row[2]} customer id {row[3]} for the {row[4]} flight in seat {row[5]}.')
+            line_count += 1
+        print(f'Processed {line_count} lines.')
+
+
+
